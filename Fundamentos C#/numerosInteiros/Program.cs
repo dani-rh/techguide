ff75a917722aaab0898b867e090d2e986e4abdb8
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-07 17:37:56 -0300

    Initial commit

---
+++ Fundamentos C#/numerosInteiros/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace numerosInteiros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////inteiros
+            //int a = 5;
+            //int b = 4;
+            //int c = 2;
+
+            //int d = a + b * c;
+
+            //Console.WriteLine(c);
+
+            ////Min e Max
+            //int max = int.MaxValue;
+            //int min = int.MinValue;
+            //Console.WriteLine($"O limite máximo da variável Integer é de {min} até {max}");
+
+            //double
+            //double a = 5;
+            //double b = 4;
+            //double c = 2;
+            //double d = (a + b) / c; ;
+            //Console.WriteLine(d);
+
+            //double f = 1.0 / 3.0;
+            //Console.WriteLine(f);
+
+            //decimais
+            decimal c = 1.0M; //necessario usar o sufixo M
+            decimal d = 3.0M;
+
+
+
+
+        }
+    }
+}
---
+++ Fundamentos C#/strings/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Interpolacao
+            //string primeiroAmigo = "Joao";
+            //string segundoAmigo = "Maria";
+
+            //Console.WriteLine($"Meus amigos sao {primeiroAmigo} e {segundoAmigo}");
+
+            //Console.WriteLine($"o nome {primeiroAmigo} tem {primeiroAmigo.Length} letras");
+            //Console.WriteLine($"o nome {segundoAmigo} tem {segundoAmigo.Length} letras");
+
+
+            ////Remover espacos
+            //string saudacao = "         Olá Mundo!      ";
+            //Console.WriteLine($"[{saudacao}]");
+
+            //string apararSaudacao = saudacao.TrimStart();
+            //Console.WriteLine($"[{apararSaudacao}]");
+
+            //apararSaudacao = saudacao.TrimEnd();
+            //Console.WriteLine($"[{apararSaudacao}]");
+
+            //apararSaudacao = saudacao.Trim();
+            //Console.WriteLine($"[{apararSaudacao}]");
+
+
+            ////Replace, ToUpper and ToLower
+            //string digaOla = "Olá Mundo!";
+            //Console.WriteLine(digaOla);
+
+            //digaOla = digaOla.Replace("Olá", "eaE");
+            //Console.WriteLine(digaOla);
+
+            //Console.WriteLine(digaOla.ToUpper());
+            //Console.WriteLine(digaOla.ToLower());
+
+
+            ////Pesquisas
+            //string letraMusica = "Voce disse tchau, e eu digo olá";
+            //Console.WriteLine(letraMusica.Contains("tchau"));
+            //Console.WriteLine(letraMusica.Contains("saudacoes"));
+
+            //Console.WriteLine(letraMusica.StartsWith("Voce"));
+            //Console.WriteLine(letraMusica.StartsWith("tchau"));
+
+            //Console.WriteLine(letraMusica.EndsWith("olá"));
+            //Console.WriteLine(letraMusica.EndsWith("tchau"));
+
+            //Console.WriteLine(letraMusica.IndexOf("tchau"));
+
+        }
+    }
+}
