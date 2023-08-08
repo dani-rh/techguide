3862e4448367c4f2fade58de249389ff4fa09db1
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-08 12:10:32 -0300

    Add loops

---
+++ Fundamentos C#/loops/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////while loop
+            //int contador = 0;
+            //while (contador <= 10)
+            //{
+            //    Console.WriteLine($"Olá mundo! O contador está em {contador}");
+            //    contador++; 
+            //}
+
+            ////do while loop
+            //int contador = 0;
+            //do
+            //{
+            //    Console.WriteLine($"Olá mundo! O contador está em {contador}");
+            //    contador++;
+            //} while (contador < 10);
+
+            //for loop
+                //inicializador  //condicional  //incremento
+            for(int contador = 0; contador < 10; contador ++)
+            {
+                Console.WriteLine($"Olá mundo! O contador está em {contador}");
+            }
+
+        }
+    }
+}
