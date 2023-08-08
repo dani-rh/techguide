8d05efccdff8ef413ba0c552878caae7076fdc3b
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-08 12:10:27 -0300

    Add condicionais

---
+++ Fundamentos C#/condicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+
+            if ((a + b > 10) && (a == b)) //as duas consicoes precisam ser true
+            {
+                Console.WriteLine("A resposta é maior do que 10.");
+                Console.WriteLine("E o primeiro número é igual ao segundo.");
+            }
+            else
+            {
+                Console.WriteLine("A resposta é menor do que 10.");
+                Console.WriteLine("E o primeiro número nao é igual ao segundo.");
+            }
+
+        }
+    }
+}
